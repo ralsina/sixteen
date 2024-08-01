c437c02a53a31a81ec0ed4e56e6ce03630e0b444
Roberto Alsina <roberto.alsina@gmail.com>
2024-08-01 13:37:06 -0300

    Beginnings of CLI for builder

--- src/main.cr
+++ src/main.cr
@@ -1,5 +1,57 @@
 require "./sixteen"
+require "docopt"
 
-puts Sixteen::DataFiles.files.map &.path
+HELP = <<-DOCOPT
+Sixteen: a Timted Themes builder
 
-puts Sixteen.theme("tokyo-night-dark").slug
+It combines a templated as defined in the Tinted Themes
+specification with a color scheme to generate a theme file.
+
+Usage:
+  sixteen (-h | --help)
+  sixteen --list
+  sixteen --info <scheme>
+  sixteen --build <template> <scheme>
+  sixteen --version
+
+Options:
+    -h --help     Show this screen.
+    --list        List available schemes.
+    --info        Show information about a scheme.
+    --build       Build theme files from a template and a scheme.
+    --version     Show version.
+DOCOPT
+
+options = Docopt.docopt(HELP, ARGV)
+
+# Handle version manually
+if options["--version"]
+  puts "Crycco #{Sixteen::VERSION}"
+  exit 0
+end
+
+if options["--list"]
+  puts "Available schemes:"
+  Sixteen::DataFiles.files.map { |f|
+    "  * #{File.basename(f.path, ".yaml")}"
+  }.sort!.each { |p|
+    puts p
+  }
+  exit 0
+end
+
+if options["--info"]
+  scheme = options["<scheme>"].as(String)
+  puts "Scheme: #{scheme}"
+  puts Sixteen.theme(scheme).to_s
+  exit 0
+end
+
+if options["--build"]
+  template = Sixteen.template options["<template>"].as(String)
+  scheme = Sixteen.theme options["<scheme>"].as(String)
+  puts "Building theme from #{scheme} using #{template}"
+  exit 0
+end
+
+puts HELP
--- src/sixteen.cr
+++ src/sixteen.cr
@@ -1,4 +1,5 @@
 require "baked_file_system"
+require "colorize"
 require "yaml"
 
 module Sixteen
@@ -31,6 +32,56 @@ module Sixteen
       }.join("").downcase.gsub(" ", "-")
       slug
     end
+
+    def context
+      data = Hash(String, Bool | String | Int32 | Float64).new
+
+      data.merge({
+        "scheme-name"                  => name,
+        "scheme-author"                => author,
+        "scheme-description"           => description,
+        "scheme-slug"                  => slug,
+        "scheme-slug-underscored"      => slug.gsub("-", "_"),
+        "scheme-system"                => system,
+        "scheme-variant"               => variant,
+        "scheme-is-#{variant}-variant" => true,
+      })
+      palette.each do |k, v|
+        data["#{k}-hex"] = v
+        data["#{k}-hex-bgr"] = v[4..5] + v[2..3] + v[..1]
+        data["#{k}-hex-r"] = v[..1]
+        data["#{k}-hex-g"] = v[2..3]
+        data["#{k}-hex-b"] = v[4..5]
+        data["#{k}-rgb-r"] = v[..1].to_i(16)
+        data["#{k}-rgb-g"] = v[2..3].to_i(16)
+        data["#{k}-rgb-b"] = v[4..5].to_i(16)
+        data["#{k}-dec-r"] = v[..1].to_i(16)/255
+        data["#{k}-dec-g"] = v[2..3].to_i(16)/255
+        data["#{k}-dec-b"] = v[4..5].to_i(16)/255
+      end
+      data
+    end
+
+    def term_palette
+      pal = ""
+      palette.each do |k, v|
+        pal += "  ".colorize.back(
+          v[4..5].to_u8(16),
+          v[2..3].to_u8(16),
+          v[0..1].to_u8(16),
+        ).to_s
+      end
+      pal
+    end
+
+    def to_s
+      doc = <<-DOC
+      Scheme: #{name}
+      Author: #{author}
+      Description: #{description}
+      Palette: #{term_palette}
+      DOC
+    end
   end
 
   def self.theme(name : String) : Theme
@@ -38,5 +89,28 @@ module Sixteen
     Theme.from_yaml(tfile.gets_to_end)
   rescue BakedFileSystem::NoSuchFileError
     raise Exception.new("Theme not found: #{name}")
+  end
+
+  struct TemplateFile
+    include YAML::Serializable
+    property systems : Array(String) = ["base16"]
+    property filename : String?
+    property extension : String?
+    property output : String?
+
+    def filename : String?
+      return @filename unless @filename.nil?
+      raise Exception.new(
+        "Template has no filename and no output and extension"
+      ) if output.nil? || extension.nil?
+      "{{#{output}}}/{{scheme-system}}-{{scheme-slug}}.{{#{extension}}}"
+    end
+  end
+
+  alias Template = Hash(String, TemplateFile)
+
+  def self.template(path : String) : Template
+    tfile = File.open("#{path}/config.yaml")
+    Template.from_yaml(tfile.gets_to_end)
   end
 end
