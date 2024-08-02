f600617d65ee6e6e3d3efc21bb34a21302f18d49
Roberto Alsina <roberto.alsina@gmail.com>
2024-08-02 11:39:00 -0300

    typo

--- src/main.cr
+++ src/main.cr
@@ -2,7 +2,7 @@ require "./sixteen"
 require "docopt"
 
 HELP = <<-DOCOPT
-Sixteen: a Timted Themes builder
+Sixteen: a Tinted Themes builder
 
 It combines a templated as defined in the Tinted Themes
 specification with a color scheme to generate a theme file.
@@ -12,13 +12,16 @@ Usage:
   sixteen --list
   sixteen --info <scheme>
   sixteen --build <template> <scheme>
+  sixteen --render <template> <scheme>
   sixteen --version
 
 Options:
     -h --help     Show this screen.
     --list        List available schemes.
     --info        Show information about a scheme.
-    --build       Build theme files from a template and a scheme.
+    --build       Build theme files from a tinted themes style template 
+                  folder and a scheme.
+    --render      Render a mustache template with a scheme and
     --version     Show version.
 DOCOPT
 
@@ -50,6 +53,16 @@ if options["--build"]
   template = Sixteen.template options["<template>"].as(String)
   scheme = Sixteen.theme options["<scheme>"].as(String)
   template.render(scheme)
+  exit 0
+end
+
+if options["--render"]
+  template = options["<template>"].as(String)
+  scheme = Sixteen.theme options["<scheme>"].as(String)
+  puts Crustache.render(
+    Crustache.parse(File.read(template)),
+    scheme.context
+  )
   exit 0
 end
 
