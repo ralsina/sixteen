6972bfc7f0dccbb8aaf82900914c4b8074ff88c3
Roberto Alsina <roberto.alsina@gmail.com>
2024-08-29 22:25:18 -0300

    build: Use hace instead of make, add release scripts, pre-commit hooks

--- build_static.sh
+++ build_static.sh
@@ -7,10 +7,10 @@ docker run --rm --privileged \
 
 # Build for AMD64
 docker build . -f Dockerfile.static -t sixteen-builder
-docker run -ti --rm -v "$PWD":/app --user="$UID" sixteen-builder /bin/sh -c "cd /app && rm -rf lib shard.lock && make static"
+docker run -ti --rm -v "$PWD":/app --user="$UID" sixteen-builder /bin/sh -c "cd /app && rm -rf lib shard.lock && shards build --static"
 mv bin/sixteen bin/sixteen-static-linux-amd64
 
 # Build for ARM64
 docker build . -f Dockerfile.static --platform linux/arm64 -t sixteen-builder
-docker run -ti --rm -v "$PWD":/app --platform linux/arm64 --user="$UID" sixteen-builder /bin/sh -c "cd /app && rm -rf lib shard.lock && make static"
+docker run -ti --rm -v "$PWD":/app --platform linux/arm64 --user="$UID" sixteen-builder /bin/sh -c "cd /app && rm -rf lib shard.lock && shards build --static"
 mv bin/sixteen bin/sixteen-static-linux-arm64
