8be8b175694dd262ee1dda888802507e4ad302c4
Roberto Alsina <roberto.alsina@gmail.com>
2025-02-18 21:37:59 -0300

    chore: fix test

--- spec/sixteen_spec.cr
+++ spec/sixteen_spec.cr
@@ -3,7 +3,7 @@ require "./spec_helper"
 describe Sixteen do
   # Test for Issue #1
   it "should not overflow" do
-    color = Sixteen::Color.new(b=199u8, g=241u8, r=251u8)
+    color = Sixteen::Color.new(b: 199u8, g: 241u8, r: 251u8)
     color.darker(0.2)
   end
 end
